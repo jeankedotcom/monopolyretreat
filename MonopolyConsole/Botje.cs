d84600971b57c9842bc0153b85b8a9517bb6b744
jeankedotcom <jan.wielemans@devgem.be>
2018-03-23 14:29:12 +0100

    more stuff

---
+++ MonopolyConsole/Botje.cs
@@ -0,0 +1,47 @@
+﻿using MonopolyRetreat.Dto;
+using MonopolyRetreat.Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MonopolyConsole
+{
+    class Botje
+    {
+        private readonly string _token;
+        private readonly MonocleClient _monocleClient;
+
+        public Botje(string token)
+        {
+            _token = token;
+            _monocleClient = new MonocleClient("http://178.62.247.230", token);
+        }
+
+        public LeaderboardUserDto GetStats()
+        {
+            return _monocleClient.GetMyOverview();
+        }
+        public void PerformNextStep()
+        {
+            var all = _monocleClient.GetAllProperties();
+            Console.WriteLine(JsonConvert.SerializeObject(all, Formatting.Indented));
+            var buyableProperties = _monocleClient.GetBuyableProperties();
+            if (buyableProperties == null) { return; }
+            if (buyableProperties.Any())
+            {
+                var buyableProperty = buyableProperties.OrderByDescending(b => b.Value).FirstOrDefault();
+                try
+                {
+                    _monocleClient.BuyProperty(buyableProperty.X, buyableProperty.Y);
+                }
+                catch (Exception e)
+                {
+                    // aankoop property is om een of andere reden niet gelukt. Eigen retry logica
+                }
+            }
+        }
+    }
+}
+
---
+++ MonopolyConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MonopolyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botje = new Botje("32ecb90722adf84b2e015425c36f78f2195601fe3378676c8384d0cebb4ae1e9");
+
+            while (true)
+            {
+                ConsoleKeyInfo key;
+                if (key != null && key.KeyChar == 's')
+                    break;
+                botje.PerformNextStep();
+                Console.WriteLine(JsonConvert.SerializeObject(botje.GetStats()));
+                key = Console.ReadKey();
+            }
+        }
+    }
+}
